e54f681a00bee113fad251dd7180a3151abe73a5
Sonali Jatav <sonalijatav972@gmail.com>
2025-11-10 23:05:15 +0530

    frontend commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'  // ✅ default import, no curly braces
 
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
